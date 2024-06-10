03697cd2cd890860b64216ee3eea802a605413ec
Jwarmflash <157661346+Jwarmflash@users.noreply.github.com>
2024-06-09 19:50:32 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -4,8 +4,7 @@ import sys
 import re
 
 for line in sys.stdin: 
-    line = line.strip() 
-    words = re.split('\w',line)
+    words = re.split('\W',line)
     for word in words:
         if word.isalpha():
           print('%s\t%s' % (word, 1))
